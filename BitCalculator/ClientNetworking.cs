d2bf978a5bb1204dd899df72a67b14ab9e10eed2
Christopher <Christopher@marvica.com.au>
2022-05-21 19:08:13 +1000

    Added networking classes

---
+++ BitCalculator/ClientNetworking.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DevTools
+{
+    public class ClientNetworking
+    {
+        private Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static byte[] _buffer = new byte[10];
+        public Func<string,bool> RecieveMessage;
+
+        public string IP;
+        public int Port;
+
+        public ClientNetworking(string IP, int Port, Func<string, bool> RecieveMessage)
+        {
+            this.IP = IP;
+            this.Port = Port;
+            this.RecieveMessage = RecieveMessage;
+            SetupClient();
+        }
+
+        public void SetupClient()
+        {
+            ConnectLoop();
+            _clientSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallback), _clientSocket);
+        }
+        private void RecieveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int recieved = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[recieved];
+            Array.Copy(_buffer, dataBuf, recieved);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            RecieveMessage(text);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallback), socket);
+        }
+        private void ConnectLoop()
+        {
+            int attempts = 0;
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    ++attempts;
+                    if (DateTime.Now.Hour >= 15)
+                    {
+                        _clientSocket.Connect(IPAddress.Parse(IP), Port);
+                    }
+                    else
+                    {
+                        _clientSocket.Connect(IPAddress.Parse(IP), Port);
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+        public void Send(byte[] data)
+        {
+            _clientSocket.Send(data);
+        }
+    }
+}
---
+++ BitCalculator/ServerNetworking.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DevTools
+{
+    class ServerNetworking
+    {
+        private readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private int Port;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+
+        public Func<string, int> RecieveMessage;
+
+        public ServerNetworking(int Port, Func<string,int> RecieveMessage)
+        {
+            this.RecieveMessage = RecieveMessage;
+            this.Port = Port;
+            //     Console.Title = "Server";
+
+            //     Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, Port));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            //      Console.WriteLine("Server setup complete");
+
+            Console.ReadLine(); // When we press enter close everything
+        }
+
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected, waiting for request...");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+        public void SendToAll(string message)
+        {
+            foreach (var client in clientSockets)
+            {
+                try
+                {
+                    client.Send(Encoding.ASCII.GetBytes(message));
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+
+}
