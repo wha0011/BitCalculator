0566d953d18d561df6d175303d5498df4820e189
Christopher <Christopher@marvica.com.au>
2022-05-24 17:49:33 +1000

    Changed the way text is modified for #define type variables

--- BitCalculator/CustomConsole.cs
+++ BitCalculator/CustomConsole.cs
@@ -268,13 +268,7 @@ namespace DevTools
                 {
                     foreach (var idx in v.AllIndexs(name))
                     {
-                        if (idx == 0 || !char.IsLetter(v[idx - 1])) //Is this the start of the word?
-                        {
-                            if (idx + name.Length >= v.Length || !char.IsLetter(v[idx + name.Length]))//Is this the end of a word?
-                            {
-                                variableLocations.Add(new FuncLocation(idx, idx + name.Length, name));
-                            }
-                        }
+                        variableLocations.Add(new FuncLocation(idx, idx + name.Length, name));
                     }
                 }
             }
